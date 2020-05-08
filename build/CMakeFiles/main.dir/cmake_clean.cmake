460536a45fb77049b168396975e6de93c700ce33
CountyRipper <1097249217@qq.com>
2020-05-08 09:07:18 +0800

    init

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.obj"
+  "CMakeFiles/main.dir/parse.cpp.obj"
+  "CMakeFiles/main.dir/scan.cpp.obj"
+  "CMakeFiles/main.dir/util.cpp.obj"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/scan
+# Build directory: E:/scan/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
